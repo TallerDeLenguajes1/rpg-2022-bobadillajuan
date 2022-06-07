7f28a0781dc76f195fb846b1c90a529cfd111a2c
Victor Molina <Victormolinalvp@gmail.com>
2022-06-07 15:17:31 -0300

    Tarberna 01

---
+++ personajes.cs
@@ -0,0 +1,15 @@
+public class Personajes{
+
+    public string Tipo;
+    public string Nombre;
+    public string Apodo;
+    public DateTime FechaDeNacimiento;
+    public int Edad;
+    public int Salud;
+    public int Velocidad;
+    public int Destreza;
+    public int Fuerza;
+    public int Nivel;
+    public int Armadura;
+
+}
